68e87a632ecfc74d49d4384ff9b3cb63d0320423
ReihanFatilla <reihanfatilla@gmail.com>
2023-09-20 16:24:56 +0700

    [feat]: added space to turn and winner text

--- app/src/main/java/com/zealsasia/reift/tictactoe/presentation/main/MainScreen.kt
+++ app/src/main/java/com/zealsasia/reift/tictactoe/presentation/main/MainScreen.kt
@@ -1,7 +1,9 @@
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.ExperimentalMaterialApi
@@ -77,6 +79,7 @@ fun WinnerOrTurnText(state: TicTacToe, modifier: Modifier = Modifier) {
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Spacer(modifier = Modifier.height(28.dp))
         Text(
             text = if(viewModel.isFinished){
                 "Winner:"
